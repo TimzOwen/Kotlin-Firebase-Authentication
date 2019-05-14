48c6e9bcccfc353d8eb33dffbaab83c41502d92b
TimzOwen <timzowen1130@gmail.com>
2019-05-13 21:32:09 -0400

    Authentication with Firebase

---
+++ app/src/main/java/com/owen/kotlinfirebaseauth/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.owen.kotlinfirebaseauth
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+//    val mAuth = FirebaseAuth.getInstance();
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
