df558988cf276321e06de0280be8db97533c0e4b
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-05-17 03:08:55 -0400

    Update LoginActivity.kt

--- app/src/main/java/com/owen/kotlinfirebaseauth/LoginActivity.kt
+++ app/src/main/java/com/owen/kotlinfirebaseauth/LoginActivity.kt
@@ -2,11 +2,31 @@ package com.owen.kotlinfirebaseauth
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+        val loginButton = findViewById<View>(R.id.loginBtn) as Button
+
+        loginButton.setOnClickListener(View.OnClickListener {
+
+             View ->  login()
+        })
+
+       
+    }
+     private fun login()
+    {
+
     }
 }
