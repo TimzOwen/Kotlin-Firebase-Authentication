fa4e15ef91dbb7448f745d1423331c244dcff606
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-05-24 05:27:25 -0400

    Update MainActivity.kt
    Register user to db

--- app/src/main/java/com/owen/kotlinfirebaseauth/MainActivity.kt
+++ app/src/main/java/com/owen/kotlinfirebaseauth/MainActivity.kt
@@ -1,16 +1,59 @@
 package com.owen.kotlinfirebaseauth
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
-class MainActivity : AppCompatActivity() {
+class Timeline : AppCompatActivity() {
 
-//    val mAuth = FirebaseAuth.getInstance();
+    lateinit var mDatabase : DatabaseReference
+    var mAuth = FirebaseAuth.getInstance()
+    var user = FirebaseAuth.getInstance().currentUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_timeline)
 
+        val nameTxt = findViewById<View>(R.id.dispTxt) as TextView
 
+        var uid = user!!.uid
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("Names")
+
+        mDatabase.child(uid).child("Name").addValueEventListener( object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError?) {
+               //coming next soon
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                nameTxt.text =  "Hey  " + snapshot.value.toString() + " ,Welcome"
+            }
+        })
+        
     }
+
+    
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item!!.itemId == R.id.signOut) {
+            mAuth.signOut()
+            Toast.makeText(this, "You Signed Out :(", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
