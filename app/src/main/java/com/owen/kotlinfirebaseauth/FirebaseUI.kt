7500953f822ac11b13d75a309c39943a4d47c255
Timz Owen <ktimothy@kabarak.ac.ke>
2019-08-09 10:24:34 -0700

    Create FirebaseUI.kt
    Updated the flow OnActivity

---
+++ app/src/main/java/com/owen/kotlinfirebaseauth/FirebaseUI.kt
@@ -0,0 +1,35 @@
+Creating signIn Intent
+val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(),
+        AuthUI.IdpConfig.PhoneBuilder().build(),
+        AuthUI.IdpConfig.GoogleBuilder().build(),
+        AuthUI.IdpConfig.FacebookBuilder().build(),
+        AuthUI.IdpConfig.TwitterBuilder().build())
+
+// Create and launch sign-in intent
+startActivityForResult(
+        AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+        RC_SIGN_IN)FirebaseUIActivity.kt
+        
+        //get onActivity REsult after login flow
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    if (requestCode == RC_SIGN_IN) {
+        val response = IdpResponse.fromResultIntent(data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+}
